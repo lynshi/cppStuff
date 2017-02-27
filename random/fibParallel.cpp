7f009318cbbbb0800879bc81b51f5489b2c9a4ab
lynshi <lyndrive@gmail.com>
2017-02-26 22:41:14 -0500

    Modified fibonacci.cpp for ease of use (only displays final answer), epicly failing at implementing parallel Fibonacci

---
+++ random/fibParallel.cpp
@@ -0,0 +1,42 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <iostream>
+
+unsigned long long calcFib(unsigned long long);
+
+int main(){
+    unsigned long long times;
+    std::cout << "Which element to display? ";
+    //std::cout << "How many elements to display? ";
+    std::cin >> times;
+
+    std::cout << calcFib(times) << std::endl;
+}
+
+unsigned long long calcFib(unsigned long long elem){
+    unsigned long long fibNum=1, buf;
+    int pipefd[2];
+
+    if(elem <= 2){
+        return 1;
+    }
+
+    pipe(pipefd);
+    pid_t pID = fork();
+
+    if(pID == 0){
+        fibNum = calcFib(elem-2);
+        std::cout << "Elem: " << elem << " Elem-2: " << fibNum << std::endl;
+        write(pipefd[1], &fibNum, sizeof(fibNum));
+        exit(0);
+    }
+    else{
+        fibNum = calcFib(elem-1);
+        std::cout << "Elem: " << elem << " Elem-1: " << fibNum << std::endl;
+        wait(NULL);
+        return fibNum += read(pipefd[0], &buf, sizeof(buf));
+    }
+}
--- random/fibonacci.cpp
+++ random/fibonacci.cpp
@@ -1,33 +1,40 @@
 #include<iostream>
 #include<vector>
 
-int calcFib(int);
+unsigned long long calcFib(unsigned long long);
 
-std::vector<int> fibNums;
+//std::vector<int> fibNums;
 
 int main(){
-    int times;
-    std::cout << "How many elements to display? ";
+    unsigned long long times;
+    std::cout << "Which element to display? ";
+    //std::cout << "How many elements to display? ";
     std::cin >> times;
 
+    /*
     for(int i = 0; i < times; i++)
         fibNums.push_back(0);
 
+
     calcFib(times);
+
 
     for(int i = 0; i < times; i++)
         std::cout << fibNums.at(i) << " ";
+    */
 
-    std::cout << std::endl;
+    std::cout << calcFib(times) << std::endl;
 }
 
-int calcFib(int elem){
-    if(elem == 1 || elem == 2){
-        fibNums.at(elem-1) = 1;
+unsigned long long calcFib(unsigned long long elem){
+    if(elem <= 2){
+        //fibNums.at(elem-1) = 1;
         return 1;
     }
 
+    return calcFib(elem-1) + calcFib(elem-2);
+
     int currentNum = calcFib(elem-1) + calcFib(elem-2);
-    fibNums.at(elem-1) = currentNum;
+    //fibNums.at(elem-1) = currentNum;
     return currentNum;
 }
