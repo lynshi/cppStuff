b78c34eb61a9911d1e30e93fea268dfab55df8d3
lynshi <lyndrive@gmail.com>
2017-02-26 23:38:44 -0500

    Reduced memory usage of fibSerial

--- random/fibSerial.cpp
+++ random/fibSerial.cpp
@@ -1,19 +1,18 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int main(){
     unsigned long long times;
-    vector<unsigned long long> fibNums;
+    unsigned long long fibNums[3];
 
     cout << "Which element would you like to display? ";
     cin >> times;
 
-    fibNums.push_back(1);
-    fibNums.push_back(1);
+    fibNums[0] = 1;
+    fibNums[1] = 1;
 
     for(unsigned long long i = 2; i < times; i++){
-        fibNums.push_back(fibNums.at(i-1) + fibNums.at(i-2));
+        fibNums[i%3] = fibNums[(i-1) % 3] + fibNums[(i-2) % 3];
     }
-    cout << fibNums[times-1] << endl;
+    cout << fibNums[(times-1)%3] << endl;
 }
