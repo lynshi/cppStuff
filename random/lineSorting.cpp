7073b205f3159be2eba92ab31b686eb3d41538ab
lynshi <lyndrive@gmail.com>
2017-02-28 20:24:13 -0500

    Saving current progress on lineSorting

--- random/lineSorting.cpp
+++ random/lineSorting.cpp
@@ -4,12 +4,12 @@
 #include<string>
 #include<ctime>
 #include<stdlib.h>
+#include<stdio.h>
 using namespace std;
 
-int main(){
-    vector<string> fileLines;
-    string temp;
+void writeToFile(string);
 
+int main(){
     ofstream outFile;
     outFile.open("testOut.txt");
     ifstream inFile;
@@ -18,20 +18,49 @@ int main(){
 
     for(int i = 0; i < 10; i += 2){
         outFile << i;
-        for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
             outFile << rand() % 10;
         }
         outFile << endl;
     }
 
     outFile.close();
+    string fullString="";
+    char buffer[10];
+    int tempInt;
+
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 3; j++){
+            sprintf(buffer, "%d", j);
+            fullString += tempString;
+        }
+        writeToFile(fullString);
+    }
+}
+
+void writeToFile(string writeThis){
+    cout << writeThis << endl;
+    return;
+    vector<string> fileLines;
+    string temp;
+
+    ofstream outFile;
+    ifstream inFile;
+
     inFile.open("testOut.txt");
 
     while(getline(inFile,temp)){
         fileLines.push_back(temp);
     }
 
+    inFile.close();
+
+
     for(int i = 0; i < fileLines.size(); i++){
-        cout << fileLines.at(i) << endl;
+        for(int j = 0; j < fileLines.at(i).length(); j++){
+            if(char(fileLines.at(i).at(j)) < char()){
+
+            }
+        }
     }
 }
